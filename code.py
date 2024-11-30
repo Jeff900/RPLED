bc64bb0308b9e261a55cb3f5642b19099fdddfbc
Jeff900 <jeffrey.spruit@gmail.com>
2024-11-30 21:33:27 +0100

    added datafile and changed code structure

--- code.py
+++ code.py
@@ -3,14 +3,22 @@ import board
 import digitalio
 import neopixel
 from rainbowio import colorwheel
+import json
 
 
 class LED:
-    def __init__(self, brightness=0.5):
+    def __init__(self):
+        self.data = self.get_data()
         self.num_pixels = 16
         self.pixels = neopixel.NeoPixel(board.GP0, self.num_pixels)
-        self.pixels.brightness = brightness
+        self.pixels.brightness = self.data['brightness']
         self.solid = Solid()
+
+    def get_data(self, filename='data.json') -> dict:
+        """Loads json data from filename and return dict."""
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        return data
 
     def set_leds(self, rgb):
         self.pixels.fill(rgb)
@@ -27,14 +35,18 @@ class LED:
 class Solid:
     def __init__(self):
         rgb = (0, 0, 0)
-        
+
     def set_rgb(self, r, g, b):
         self.rgb = (r, g, b)
 
 
-led = LED(brightness=0.1)
+def main():
+    while True:
+        led = LED()
+        led.set_leds(led.data['rgb'])
+        rgb = input('Insert RGB values seperated by commas: ')
+        rgb = [int(i) for i in rgb.split(',')]
+        led.set_leds(rgb)
 
-while True:
-    rgb = input('Insert RGB values seperated by commas: ')
-    rgb = [int(i) for i in rgb.split(',')]
-    led.set_leds(rgb)
+if __name__ == '__main__':
+    main()
